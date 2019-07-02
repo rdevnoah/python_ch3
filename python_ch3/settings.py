4da5984d3ca272a5191e127a40388e4452671dce
rdevnoah <zzagam2@gmail.com>
2019-07-02 14:45:36 +0900

    allows host settings.py

--- python_ch3/settings.py
+++ python_ch3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+12yzs-(vpgko4f)#&8pz+w^g2%f+sgz53d6u(ez7t(n3mat&p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
